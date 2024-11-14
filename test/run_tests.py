1aef81c25a4b4f694dee039e313f43ff88a2ce36
Kerry Goodwine <cgoodwine@volexity.com>
2024-11-14 13:54:45 -0500

    Add test template for plugins

---
+++ test/run_tests.py
@@ -0,0 +1,237 @@
+# ASSUMPTIONS: 
+#   1. all existing plugin tests are contained in test_volatility.py 
+#   2. all existing plugin tests are functions named 'test_[windows OR mac OR linux OR misc]_FILENAME'
+#     2.1 ex: test_windows_psslist means it tests all classes in the volatility3/framework/plugins/windows/psslist.py file
+#   3. all plugins can be found calling framework.list_plugins()
+#   4. all plugins are named [windows OR mac OR linux OR FILENAME (for misc)].FILENAME+.CLASSNAME (except for the misc plugins) (FILENAME+ means there can be multiple directories
+#      between [windows OR mac OR linux] and the classname; the only one that actually exists as of now is windows/registry/)
+#     4.1 ex: windows.mftscan.MFTScan or windows.mftscan.ADS means that volatility3/framework/plugins/windows has a file named 'mftscan.py' and that has (at least) two classes named MFTScan and ADS
+#     4.2 ex: banners.Banners means that volatility3/framework/plugins/ has a file named 'banners.py' and that has (at least) one class named Banners
+#     4.3 ex: windows.registry.hivelist.HiveList means that volatility3/framework/plugins/windows/registry/hivelist has (at least) one class named HiveList
+#       4.3.1 ex: in cases like this, the test would be named after the LAST filename in the list (hivelist, not registry)
+#   5. all plugins can be found under volatility3/framework/plugins/[windows OR mac OR linux OR nothing for misc]
+#     5.1: this needs to be updated; forgot that there was a volatility3/plugins/[windows OR mac OR linux] case; it does seem like the only one to actually use this case is windows (mac and linux
+#          just have __init__.py in that folder but still should check it in case that changes
+          
+
+
+# Options for inputs to plugins:
+#  1. no inputs
+#  2. one input: either interfaces.plugin.PluginInterface OR plugins.PluginInterface
+#    2a. one input: unique (?? maybe works maybe not; seems ok for some of them but still fine-tuning)
+#  3. two inputs: (either interfaces.plugin.PluginInterface OR plugins.PluginInterface) combined with (timeliner.TimelinerInterface)
+
+import os
+import sys
+import subprocess
+sys.path.append("/Users/cgoodwine/src/gitlab.research.volexity.com/volatility3")
+
+import volatility3.plugins
+from volatility3 import framework
+from volatility3.framework import (
+  automagic,
+  contexts,
+  plugins,
+)
+
+def runvol(args, volatility, python):
+    volpy = volatility
+    python_cmd = python
+
+    cmd = [python_cmd, volpy] + args
+   # print(" ".join(cmd))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    #print("stdout:")
+    #sys.stdout.write(str(stdout))
+    #print("")
+
+    return p.returncode
+
+
+def runvol_plugin(plugin, img, volatility, python, pluginargs=[], globalargs=[]):
+    args = (
+        globalargs
+        + [
+            "--single-location",
+            img,
+            "-q",
+            plugin,
+        ]
+        + pluginargs
+    )
+
+    return runvol(args, volatility, python)
+
+
+class Plugin:
+  def __init__(self, os, directories, file_name, class_name, full_name, inputs):
+    self.os = os
+    self.directories = directories
+    self.file_name = file_name
+    self.class_name = class_name
+    self.full_name = full_name
+    self.inputs = inputs
+
+def sort_plugins(plugins):
+  results = []
+  
+  for plugin in plugins:
+    full_name = plugin
+    count = plugin.count('.')
+    if count == 1:
+      p = Plugin("misc", "", plugin[:plugin.find('.')], plugin[plugin.find('.')+1:], full_name, [])
+      results.append(p)
+    elif count == 2:
+      os = plugin[:plugin.find('.')]
+      file_name = plugin[plugin.find('.')+1:plugin.rfind('.')]
+      class_name = plugin[plugin.rfind('.')+1:]
+      p = Plugin(os, "", file_name, class_name, full_name, [])
+      results.append(p)
+    elif count == 3:
+      os = plugin[:plugin.find('.')]
+      class_name = plugin[plugin.rfind('.')+1:]
+      plugin = plugin[:plugin.rfind('.')]
+      file_name = plugin[plugin.rfind('.')+1:]
+      directories = plugin[plugin.find('.')+1:plugin.rfind('.')]
+      p = Plugin(os, directories, file_name, class_name, full_name, [])
+      results.append(p)
+    else:
+      print("big problem")
+        
+  return results
+
+
+def get_inputs(plugins):
+  relative_path = "volatility3/framework/plugins/"
+  for plugin in plugins:
+    # build path
+    full_path = relative_path
+    if plugin.os != "misc":
+      full_path += (plugin.os + "/")
+    if plugin.directories != "":
+      continue # SKIP NESTED DIRECTORIES FOR NOW
+    full_path += (plugin.file_name + '.py')
+    
+    try:
+      with open(full_path, 'r') as file:
+        # find the classes
+        for line in file:
+          if "class " + plugin.class_name + "(" in line:
+            inputs = line[line.find('(')+1:line.find(')')].split(', ')
+            for i in inputs:
+              plugin.inputs.append(i)
+    except Exception:
+      print("couldn't find path:", full_path)
+      del plugins[plugins.index(plugin)]
+
+
+def find_existing_tests(file_name):
+  test_names = []
+
+  with open("test/" + file_name, 'r') as f:
+    for line in f:
+      if 'def' in line and 'test' in line:
+        func_name = line[line.find('test'):line.find('(')]
+        plugin = Plugin(func_name[func_name.find('_')+1:func_name.find('_', func_name.find('_')+1)], '', '', '', func_name[func_name.find('_', func_name.find('_')+1)+1:], [])
+        test_names.append(plugin)
+        
+  return test_names
+
+def main():
+  
+  # open test_volatility
+  windows_tests = []
+  windows_func_start = "test_windows_"
+  mac_tests = []
+  mac_func_start = "test_mac_"
+  linux_tests = []
+  linux_func_start = "test_linux_"
+  misc_tests = []
+  misc_func_start = "test_misc_"
+  with open("test/test_volatility2.py", "r") as file:
+    for line in file:
+      if 'def' in line:
+#        print(line, end='')
+        def_find = line.find('def')
+        open_parenthesis_find = line.find("(")
+        func_name = line[def_find+4:open_parenthesis_find]
+        if 'test' in func_name:
+          if 'windows' in func_name:
+            windows_tests.append(func_name[len(windows_func_start):])
+          if 'mac' in func_name:
+            mac_tests.append(func_name[len(mac_func_start):])
+          if 'linux' in func_name:
+            linux_tests.append(func_name[len(linux_func_start):])
+          if 'windows' not in func_name and 'mac' not in func_name and 'linux' not in func_name:
+            misc_tests.append(func_name[len(misc_func_start):])
+
+  # COPIED FROM VOLATILITY !!!
+  # Do the initialization
+  ctx = contexts.Context()  # Construct a blank context
+  failures = framework.import_files(
+      volatility3.plugins, True
+  )  # Will not log as console's default level is WARNING
+  if failures:
+      parser.epilog = (
+          "The following plugins could not be loaded (use -vv to see why): "
+          + ", ".join(sorted(failures))
+      )
+      vollog.info(parser.epilog)
+  automagics = automagic.available(ctx)
+
+  plugins = framework.list_plugins()
+
+  all_plugins = sort_plugins(plugins)
+
+  get_inputs(all_plugins)
+  found_tests = find_existing_tests('test_volatility2.py')
+
+  extra_weird_count = 0
+  kinda_weird_count = 0
+  normal_count = 0
+  needs_test = []
+  have_test = []
+  for plugin in all_plugins:
+    for test in found_tests:
+      if plugin.class_name.lower() in test.full_name and plugin.os == test.os:
+        have_test.append(plugin)
+        break
+    if plugin not in have_test:
+      needs_test.append(plugin)
+
+  for plugin in have_test:
+    print('have test for', plugin.file_name, plugin.os)
+
+  volatility='vol.py'
+  image=sys.argv[1]
+  python='python3'
+  need_parameters = []
+  failed = []
+  for plugin in needs_test:
+    return_code = -1
+    if plugin.os != 'windows':
+      continue
+    if len(plugin.inputs) == 1:
+     # if ('interfaces.plugins.PluginInterface' not in plugin.inputs and 'plugins.PluginInterface' not in plugin.inputs):
+     #   print("INPUTS:", plugin.inputs, 'for plugin', plugin.os + '.' + plugin.file_name + '.' + plugin.class_name)
+     # else:
+        return_code = runvol_plugin(plugin.os + '.' + plugin.file_name + '.' + plugin.class_name, image, volatility, python)
+    if len(plugin.inputs) == 2:
+     # if (('interfaces.plugins.PluginInterface' not in plugin.inputs and 'plugins.PluginInterface' not in plugin.inputs) or 'timeliner.TimeLinerInterface' not in plugin.inputs):
+     #   print("INPUTS:", plugin.inputs, 'for plugin', plugin.os + '.' + plugin.file_name + '.' + plugin.class_name)
+     # else:
+        return_code = runvol_plugin(plugin.os + '.' + plugin.file_name + '.' + plugin.class_name, image, volatility, python)
+    if return_code != -1:
+      if return_code == 1:
+        failed.append(plugin)
+      elif return_code == 2:
+        need_parameters.append(plugin)
+      elif return_code != 0:
+        print("weird return code {return_code} for plugin {plugin.class_name}")
+    
+  print("failed:", failed)
+  print("need parameters:", need_parameters)
+
+if __name__ == '__main__':
+  main()
